ef0dbdbeb17b6ce8b419a76b9332d7a4ea92c718
boblox <boblox12@gmail.com>
2014-07-16 17:07:48 +0300

    Fix for menu and pages

--- WebSite/js/config.js
+++ WebSite/js/config.js
@@ -5,11 +5,22 @@
 */
 
 window._skel_config = {
-	preset: 'standard',
-	prefix: '/css/style',
-	resetCSS: true
+    preset: 'standard',
+    prefix: '/css/style',
+    resetCSS: true
 };
 
 window._skel_panels_config = {
-	preset: 'standard'
+    preset: 'standard'
 };
+
+jQuery(function () {
+    jQuery('#nav > ul').dropotron({
+        offsetY: -20,
+        offsetX: -1,
+        mode: 'fade',
+        noOpenerFade: true,
+        alignment: 'center',
+        detach: false
+    });
+});
